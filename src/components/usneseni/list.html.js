4c4ded8d1772a7e7729758d06bdebc42cea72ba7
vencax <vencax@centrum.cz>
2020-10-26 19:50:39 +0100

    usneseni a prilohy v tabulce

--- src/components/usneseni/list.html.js
+++ src/components/usneseni/list.html.js
@@ -2,24 +2,31 @@ export default `
 <div class="row">
 
   <div class="col-sm-12">
-    <h2>Usnesení</h2>
+    <h2 class="float-left mr-3">Usnesení</h2>
     <b-button v-if="$store.getters.isMember('taskadmin')" variant="primary" @click="add">
       <i class="fas fa-plus"></i> Přidat
     </b-button>
   </div>
 
   <div class="col-sm-12">
-    <ol>
-      <li v-for="u in usneseni">
-        {{ u.akce }}: {{ u.text }}
-        <b-button v-if="$store.getters.isMember('taskadmin')" variant="secondary" @click="edit(u)">
-          <i class="fas fa-plus"></i> Upravit
-        </b-button>
-        <b-button v-if="$store.getters.isMember('taskadmin')" variant="danger" @click="remove(u)">
-          <i class="fas fa-minus"></i> Odstranit
-        </b-button>
-      </li>
-    </ol>
+    <table class="table">
+      <tbody>
+        <tr v-for="u in usneseni">
+        <td>{{ u.akce }}</td>
+          <td>{{ u.text }}</td>
+          <td>
+            <b-button v-if="$store.getters.isMember('taskadmin')" 
+              size="sm" variant="secondary" @click="edit(u)">
+              <i class="fas fa-plus"></i> Upravit
+            </b-button>
+            <b-button v-if="$store.getters.isMember('taskadmin')"
+              size="sm" variant="danger" @click="remove(u)">
+              <i class="fas fa-minus"></i> Odstranit
+            </b-button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 
   <b-modal size="xl" id="modal-add-usneseni" title="Upravit" hide-footer>
