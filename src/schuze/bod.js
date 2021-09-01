a0527cd6dce571e8e4f83ef987ecf34f3195eea1
vencax <vencax@centrum.cz>
2021-09-01 20:47:56 +0200

    nastrel hlasovani

--- src/schuze/bod.js
+++ src/schuze/bod.js
@@ -1,15 +1,15 @@
 /* global axios, API, _ */
 import { NameSpan } from '../_shared/user.js'
 import MarkDown from '../_shared/markdown.js'
+import Hlasovani from './hlasovani.js'
 import { ROUTE_NAMES } from '../consts.js'
-// import formconfig from './formconfig.js'
 
 export default {
   data: () => {
     return {
       loading: true,
       prilohy: null,
-      usneseni: null
+      hlasovani: null
     }
   },
   async created () {
@@ -21,28 +21,34 @@ export default {
       }),
       this.$store.dispatch('send', {
         method: 'get',
-        url: `${api}/usneseni/?filter={"idbod":"${this.bod.id}"}`
+        url: `${api}/hlasovani/?filter={"idbod":"${this.bod.id}"}`
       })
     ])
     this.$data.prilohy = res[0].data
-    this.$data.usneseni = res[1].data
+    this.$data.hlasovani = res[1].data
     this.$data.loading = false
   },
   props: ['query', 'cfg', 'bod'], 
-  components: { NameSpan, MarkDown },
+  components: { NameSpan, MarkDown, Hlasovani },
   template: `
     <div class="row">
       <div class="col-sm-12 col-md-6">  
         Předkládá: <NameSpan :uid="bod.predkl" :cfg="cfg" /><br/>
         Zpracoval: <NameSpan :uid="bod.zprac" :cfg="cfg" /><br/>
         Důvodová zpráva: <b-button size="sm" v-b-modal.zprava-modal>zobrazit</b-button>
-        <b-modal id="zprava-modal" size="lg" title="Důvodová zpráva" hide-footer>
+        <b-modal id="zprava-modal" size="xl" title="Důvodová zpráva" hide-footer>
           <MarkDown :text="bod.duvod" />
         </b-modal>
 
         <h5>přílohy</h5>
+        <div class="d-inline-flex">
+          <a v-for="i,idx in prilohy" :key="idx" 
+            :href="i.name" target="_blank" class="p-1">{{ i.name }}</a>
+        </div>        
+      </div>
 
-        <h5>usnesení</h5>
+      <div class="col-sm-12 col-md-6">
+        <Hlasovani v-for="i,idx in hlasovani" :key="idx" :cfg="cfg" :hlasovani="i" />
       </div>
     </div>
   `
